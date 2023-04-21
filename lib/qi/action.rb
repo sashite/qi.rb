db7be60501dda54372fe780ee647dbdd6384bcbc
Cyril Kato <contact@cyril.email>
2023-04-21 22:39:16 +0200

    perf(Qi::Action): remove diffs' loop

--- lib/qi/action.rb
+++ lib/qi/action.rb
@@ -16,33 +16,20 @@ module Qi
     #
     # @return [Array] An action to change the position.
     def call(**diffs)
-      captures = @captures
-      squares  = @squares
+      capture = diffs.delete(CAPTURE_CHAR)
+      drop    = diffs.delete(DROP_CHAR)
 
-      diffs.each do |k, v|
-        case String(k)
-        when CAPTURE_CHAR
-          captures = capture(v, *captures)
-        when DROP_CHAR
-          captures = drop(v, *captures)
-        else
-          squares = squares.merge(k => v)
-        end
-      end
-
-      [captures, squares.compact]
+      change(*@captures, capture:, drop:, **diffs)
     end
 
     private
 
-    def capture(item, *items)
-      [item] + items
-    end
+    def change(*captures, capture: nil, drop: nil, **squares)
+      captures.unshift(capture) unless capture.nil?
+      captures.delete(drop) { |key| raise ::IndexError, "Capture #{key.inspect} not found!" } unless drop.nil?
+      squares = @squares.merge(squares).compact
 
-    def drop(item, *items)
-      item_id = items.index(item) || raise(::IndexError, "Piece #{item.inspect} not found!")
-      items.delete_at(item_id)
-      items
+      [captures, squares]
     end
   end
 end
