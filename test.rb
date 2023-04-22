af9bf627fcbd6a47171957486b2e9c4bd35dd768
Cyril Kato <contact@cyril.email>
2023-04-22 23:05:39 +0200

    feat: remove Position class

--- test.rb
+++ test.rb
@@ -7,102 +7,113 @@ require "simplecov"
 
 require "./lib/qi"
 
-STARTING_POSITION = Qi::Position.new(
-  "l", "n", "s", "g", "k", "g", "s", "n", "l",
-  nil, "r", nil, nil, nil, nil, nil, "b", nil,
-  "p", "p", "p", "p", "p", "p", "p", "p", "p",
-  nil, nil, nil, nil, nil, nil, nil, nil, nil,
-  nil, nil, nil, nil, nil, nil, nil, nil, nil,
-  nil, nil, nil, nil, nil, nil, nil, nil, nil,
-  "P", "P", "P", "P", "P", "P", "P", "P", "P",
-  nil, "B", nil, nil, nil, nil, nil, "R", nil,
-  "L", "N", "S", "G", "K", "G", "S", "N", "L"
-)
+STARTING_POSITION = {
+  side_id: 0,
+  board: {
+     0 => "l",
+     1 => "n",
+     2 => "s",
+     3 => "g",
+     4 => "k",
+     5 => "g",
+     6 => "s",
+     7 => "n",
+     8 => "l",
+    10 => "r",
+    16 => "b",
+    18 => "p",
+    19 => "p",
+    20 => "p",
+    21 => "p",
+    22 => "p",
+    23 => "p",
+    24 => "p",
+    25 => "p",
+    26 => "p",
+    54 => "P",
+    55 => "P",
+    56 => "P",
+    57 => "P",
+    58 => "P",
+    59 => "P",
+    60 => "P",
+    61 => "P",
+    62 => "P",
+    64 => "B",
+    70 => "R",
+    72 => "L",
+    73 => "N",
+    74 => "S",
+    75 => "G",
+    76 => "K",
+    77 => "G",
+    78 => "S",
+    79 => "N",
+    80 => "L"
+  },
+  hands: [[], []]
+}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", "g", "k", "g", "s", "n", "l", nil, "r", nil, nil, nil, nil, nil, "b", nil, "p", "p", "p", "p", "p", "p", "p", "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", "P", "P", "P", "P", "P", "P", "P", "P", nil, "B", nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [[], []]
-raise if actual.active_side_id != 0
+raise if actual.itself != {:side_id=>0, :board=>{0=>"l", 1=>"n", 2=>"s", 3=>"g", 4=>"k", 5=>"g", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 24=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 56=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 64=>"B", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L"}, :hands=>[[], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", "g", "k", "g", "s", "n", "l", nil, "r", nil, nil, nil, nil, nil, "b", nil, "p", "p", "p", "p", "p", "p", "p", "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, "B", nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [[], []]
-raise if actual.active_side_id != 1
+raise if actual.itself != {:side_id=>1, :board=>{0=>"l", 1=>"n", 2=>"s", 3=>"g", 4=>"k", 5=>"g", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 24=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 64=>"B", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P"}, :hands=>[[], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", nil, "k", "g", "s", "n", "l", nil, "r", "g", nil, nil, nil, nil, "b", nil, "p", "p", "p", "p", "p", "p", "p", "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, "B", nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [[], []]
-raise if actual.active_side_id != 0
+raise if actual.itself != {:side_id=>0, :board=>{0=>"l", 1=>"n", 2=>"s", 4=>"k", 5=>"g", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 24=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 64=>"B", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g"}, :hands=>[[], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", nil, "k", "g", "s", "n", "l", nil, "r", "g", nil, nil, nil, nil, "b", nil, "p", "p", "p", "p", "p", "p", "+B", "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, nil, nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [["P"], []]
-raise if actual.active_side_id != 1
+raise if actual.itself != {:side_id=>1, :board=>{0=>"l", 1=>"n", 2=>"s", 4=>"k", 5=>"g", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 24=>"+B", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g"}, :hands=>[["P"], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != ["P"]
-raise if actual.squares != ["l", "n", "s", nil, "k", nil, "s", "n", "l", nil, "r", "g", nil, nil, "g", nil, "b", nil, "p", "p", "p", "p", "p", "p", "+B", "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, nil, nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [["P"], []]
-raise if actual.active_side_id != 0
+raise if actual.itself != {:side_id=>0, :board=>{0=>"l", 1=>"n", 2=>"s", 4=>"k", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 24=>"+B", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g", 14=>"g"}, :hands=>[["P"], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", nil, "k", nil, "s", "n", "l", nil, "r", "g", nil, nil, "+B", nil, "b", nil, "p", "p", "p", "p", "p", "p", nil, "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, nil, nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [["P", "G"], []]
-raise if actual.active_side_id != 1
+raise if actual.itself != {:side_id=>1, :board=>{0=>"l", 1=>"n", 2=>"s", 4=>"k", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g", 14=>"+B"}, :hands=>[["P", "G"], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"], [4, 3, "k"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"], [4, 3, "k"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != ["P", "G"]
-raise if actual.squares != ["l", "n", "s", "k", nil, nil, "s", "n", "l", nil, "r", "g", nil, nil, "+B", nil, "b", nil, "p", "p", "p", "p", "p", "p", nil, "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, nil, nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [["P", "G"], []]
-raise if actual.active_side_id != 0
+raise if actual.itself != {:side_id=>0, :board=>{0=>"l", 1=>"n", 2=>"s", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g", 14=>"+B", 3=>"k"}, :hands=>[["P", "G"], []]}
 
 # ------------------------------------------------------------------------------
 
 actual = begin
-  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"], [4, 3, "k"], [nil, 13, "G"]].reduce(STARTING_POSITION) { |position, move| position.call(move) }
+  [[56, 47, "P"], [3, 11, "g"], [64, 24, "+B", "P"], [5, 14, "g"], [24, 14, "+B", "G"], [4, 3, "k"], [nil, 13, "G"]].reduce(STARTING_POSITION) { |position, move| Qi.call(move, **position) }
 end
 
-raise if actual.in_hand_pieces != []
-raise if actual.squares != ["l", "n", "s", "k", nil, nil, "s", "n", "l", nil, "r", "g", nil, "G", "+B", nil, "b", nil, "p", "p", "p", "p", "p", "p", nil, "p", "p", nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, "P", nil, nil, nil, nil, nil, nil, "P", "P", nil, "P", "P", "P", "P", "P", "P", nil, nil, nil, nil, nil, nil, nil, "R", nil, "L", "N", "S", "G", "K", "G", "S", "N", "L"]
-raise if actual.pieces_in_hand_grouped_by_sides != [["P"], []]
-raise if actual.active_side_id != 1
+raise if actual.itself != {:side_id=>1, :board=>{0=>"l", 1=>"n", 2=>"s", 6=>"s", 7=>"n", 8=>"l", 10=>"r", 16=>"b", 18=>"p", 19=>"p", 20=>"p", 21=>"p", 22=>"p", 23=>"p", 25=>"p", 26=>"p", 54=>"P", 55=>"P", 57=>"P", 58=>"P", 59=>"P", 60=>"P", 61=>"P", 62=>"P", 70=>"R", 72=>"L", 73=>"N", 74=>"S", 75=>"G", 76=>"K", 77=>"G", 78=>"S", 79=>"N", 80=>"L", 47=>"P", 11=>"g", 14=>"+B", 3=>"k", 13=>"G"}, :hands=>[["P"], []]}
